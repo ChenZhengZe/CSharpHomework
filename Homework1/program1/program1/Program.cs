37363a8db348a50467a8405bbbcb3fa161d52654
ChenZhengZe <2770433653@qq.com>
2018-09-16 13:37:34 +0800

    the first C#homework abnormal input detection

--- Homework1/program1/program1/Program.cs
+++ Homework1/program1/program1/Program.cs
@@ -13,12 +13,20 @@ namespace program1
             int n1 = 0;
             int n2 = 0;
             Console.Write("Please input the first number : ");
-            s = Console.ReadLine();
-            n1 = Int32.Parse(s);
-            Console.Write("Please input the second number : ");
-            s = Console.ReadLine();
-            n2 = Int32.Parse(s);
-            Console.WriteLine("You have entered : " + n1 + " and " + n2 + " , and " + n1 + " * " + n2 + " = " + (n1 * n2));
+
+            try
+            {
+                s = Console.ReadLine();
+                n1 = Int32.Parse(s);
+                Console.Write("Please input the second number : ");
+                s = Console.ReadLine();
+                n2 = Int32.Parse(s);
+                Console.WriteLine("You have entered : " + n1 + " and " + n2 + " , and " + n1 + " * " + n2 + " = " + (n1 * n2));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("input error" + e.Message);
+            }
         }
     }
 }
